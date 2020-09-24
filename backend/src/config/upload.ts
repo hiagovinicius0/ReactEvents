0f66d5f5b5e03f75596b9db4b1c0f93c3e4f00a3
hiagovinicius0 <hiago149@gmail.com>
2020-09-24 20:09:39 -0300

    update file avatar

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -10,7 +10,8 @@ export default {
         destination: tmpFolder,
         filename(request, file, callback) {
             const fileHash = crypto.randomBytes(10).toString('hex');
-            const fileName = `${fileHash}-${file.originalname}`;
+            const [,extension] = file.originalname.split('.');
+            const fileName = `${fileHash}.${extension}`;
             return callback(null, fileName);
         }
     })
